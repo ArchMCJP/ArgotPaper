092eac31a43847ef4d2c6e3f6cc300579f71b0d1
ArchMCJP <91055781+ArchMCJP@users.noreply.github.com>
2021-12-20 23:34:01 +0900

    Update applyPatches.sh

--- applyPatches.sh
+++ applyPatches.sh
@@ -2,7 +2,6 @@
 
 PS1="$"
 basedir=`pwd`
-gpgsign=$(git config commit.gpgsign)
 echo "Rebuilding Forked projects.... "
 
 function applyPatch {
@@ -11,50 +10,40 @@ function applyPatch {
     branch=$3
     cd "$basedir/$what"
     git fetch
-    git branch -f upstream "$branch" >/dev/null
+    git reset --hard "$branch"
+    git branch -f upstream >/dev/null
 
     cd "$basedir"
     if [ ! -d  "$basedir/$target" ]; then
-        git clone "$what" "$target"
+        git clone $1 $target -b upstream
     fi
     cd "$basedir/$target"
     echo "Resetting $target to $what..."
-    git remote add -f upstream ../$what >/dev/null 2>&1
+    git remote rm upstream 2>/dev/null 2>&1
+    git remote add upstream ../$what >/dev/null 2>&1
     git checkout master >/dev/null 2>&1
     git fetch upstream >/dev/null 2>&1
     git reset --hard upstream/upstream
     echo "  Applying patches to $target..."
-    git am --abort >/dev/null 2>&1
-    git am --3way --ignore-whitespace "$basedir/${what}-Patches/"*.patch
+    git am --abort
+    git am --3way "$basedir/${what}-Patches/"*.patch
     if [ "$?" != "0" ]; then
         echo "  Something did not apply cleanly to $target."
         echo "  Please review above details and finish the apply then"
         echo "  save the changes with rebuildPatches.sh"
-        enableCommitSigningIfNeeded
         exit 1
     else
         echo "  Patches applied cleanly to $target"
     fi
 }
 
-function enableCommitSigningIfNeeded {
-    if [[ "$gpgsign" == "true" ]]; then
-        echo "Re-enabling GPG Signing"
-        # Yes, this has to be global
-        git config --global commit.gpgsign true
-    fi
-}
+cd ../Bukkit
+hash=$(git rev-parse HEAD)
+git branch -f spigot "$hash"
 
-# Disable GPG signing before AM, slows things down and doesn't play nicely.
-# There is also zero rational or logical reason to do so for these sub-repo AMs.
-# Calm down kids, it's re-enabled (if needed) immediately after, pass or fail.
-if [[ "$gpgsign" == "true" ]]; then
-    echo "_Temporarily_ disabling GPG signing"
-    git config --global commit.gpgsign false
-fi
+applyPatch Bukkit Spigot-API origin/spigot && applyPatch CraftBukkit Spigot-Server origin/patched
+applyPatch Spigot-Server Migot-Server master
 
 applyPatch Bukkit Spigot-API HEAD && applyPatch CraftBukkit Spigot-Server patched
 applyPatch Spigot-API PaperSpigot-API HEAD && applyPatch Spigot-Server PaperSpigot-Server HEAD
 applyPatch PaperSpigot-API TacoSpigot-API HEAD && applyPatch PaperSpigot-Server TacoSpigot-Server HEAD
-
-enableCommitSigningIfNeeded
